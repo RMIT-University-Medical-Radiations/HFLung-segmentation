024854c5c9a195be9665d8af93546568f5debf61
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2023-10-12 07:58:38 +1100

    Update nnUNetTrainerDA5_60epochs.py

--- nnUNetTrainerDA5_60epochs.py
+++ nnUNetTrainerDA5_60epochs.py
@@ -2,7 +2,7 @@ import torch
 from nnunetv2.training.nnUNetTrainer.variants.data_augmentation.nnUNetTrainerDA5 import nnUNetTrainerDA5
 
 
-class nnUNetTrainerDA5_100epochs(nnUNetTrainerDA5):
+class nnUNetTrainerDA5_60epochs(nnUNetTrainerDA5):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda')):
         super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
         self.num_epochs = 60
