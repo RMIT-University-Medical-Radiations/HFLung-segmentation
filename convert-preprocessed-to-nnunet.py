92ad29bd374d03944785023b1388e89d60f1a41d
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2023-10-23 14:49:05 +1100

    Update convert-preprocessed-to-nnunet.py

--- convert-preprocessed-to-nnunet.py
+++ convert-preprocessed-to-nnunet.py
@@ -11,100 +11,116 @@ nnUNet_raw='/home/daryl/datasets/RNSH_HFlung/nnU-Net-processing/nnUNet_raw'
 data_dir = '/home/daryl/datasets/RNSH_HFlung/training-set'
 task_id = 138
 task_name = "RNSH_HFlung"
+
+patient_ids = np.arange(1,20+1)
+number_of_test_sets = 5
+patient_test_sets = []  # seeded with an initial set
 number_of_test_patients = 2
 
-foldername = 'Dataset{:03d}_{}'.format(task_id, task_name)
+def choose_test_patients():
+    result = []
+    while len(patient_test_sets) < number_of_test_sets:
+        s = random.sample(list(patient_ids), number_of_test_patients)
+        if (s not in patient_test_sets) and (s[::-1] not in patient_test_sets):
+            patient_test_sets.append(s)
+        else:
+            print('{} is already present'.format(s))
+    return result
 
-# setting up nnU-Net folders
-out_base_dir = '{}/{}'.format(nnUNet_raw, foldername)
-training_image_dir = '{}/{}'.format(out_base_dir, "imagesTr")
-training_label_dir = '{}/{}'.format(out_base_dir, "labelsTr")
-test_image_dir = '{}/{}'.format(out_base_dir, "imagesTs")
-test_label_dir = '{}/{}'.format(out_base_dir, "labelsTs")
+patient_test_sets = choose_test_patients()
+for test_set_idx,test_set in enumerate(patient_test_sets):
+    foldername = 'Dataset{:03d}_{}_{}'.format(task_id+test_set_idx, task_name, test_set_idx+1)
+    print('experiment {}'.format(foldername))
 
-if os.path.exists(training_image_dir):
-    shutil.rmtree(training_image_dir)
-if os.path.exists(training_label_dir):
-    shutil.rmtree(training_label_dir)
+    # setting up nnU-Net folders
+    out_base_dir = '{}/{}'.format(nnUNet_raw, foldername)
+    training_image_dir = '{}/{}'.format(out_base_dir, "imagesTr")
+    training_label_dir = '{}/{}'.format(out_base_dir, "labelsTr")
+    test_image_dir = '{}/{}'.format(out_base_dir, "imagesTs")
+    test_label_dir = '{}/{}'.format(out_base_dir, "labelsTs")
 
-if os.path.exists(test_image_dir):
-    shutil.rmtree(test_image_dir)
-if os.path.exists(test_label_dir):
-    shutil.rmtree(test_label_dir)
+    if os.path.exists(training_image_dir):
+        shutil.rmtree(training_image_dir)
+    if os.path.exists(training_label_dir):
+        shutil.rmtree(training_label_dir)
 
-os.makedirs(training_image_dir)
-os.makedirs(training_label_dir)
-os.makedirs(test_image_dir)
-os.makedirs(test_label_dir)
+    if os.path.exists(test_image_dir):
+        shutil.rmtree(test_image_dir)
+    if os.path.exists(test_label_dir):
+        shutil.rmtree(test_label_dir)
 
-path = '{}/Patient*.npy'.format(data_dir)
-file_l = sorted(glob.glob(path))
-# test_file_l = random.sample(file_l, number_of_test_patients)
-test_file_l = ["/home/daryl/datasets/RNSH_HFlung/training-set/Patient17.npy", "/home/daryl/datasets/RNSH_HFlung/training-set/Patient19.npy"]
-training_file_l = list(set(file_l) - set(test_file_l))
+    os.makedirs(training_image_dir)
+    os.makedirs(training_label_dir)
+    os.makedirs(test_image_dir)
+    os.makedirs(test_label_dir)
 
-patient_map_d = {'training':[], 'test':[]}
+    path = '{}/Patient*.npy'.format(data_dir)
+    file_l = sorted(glob.glob(path))
+    test_file_l = random.sample(file_l, number_of_test_patients)
+    training_file_l = list(set(file_l) - set(test_file_l))
 
-print('processing training set')
-for c,f in enumerate(training_file_l):
-    print(f)
-    # split the patient file contents into channels
-    patient_arr = np.load(f)
-    patient_arr = np.moveaxis(patient_arr, 1, -1)  # move the z-axis to be last
-    
-    exh_img = nib.Nifti1Image(patient_arr[0], np.eye(4))  # identity matrix for transform
-    inh_img = nib.Nifti1Image(patient_arr[1], np.eye(4))
-    label_img = nib.Nifti1Image(patient_arr[4], np.eye(4))
-    
-    case_id_str = '{}_{:04d}'.format(task_name, c)
-    
-    # 2-channel inputs
-    exh_img.to_filename('{}/{}_{:04d}.nii.gz'.format(training_image_dir, case_id_str, 0))  # channel 0
-    inh_img.to_filename('{}/{}_{:04d}.nii.gz'.format(training_image_dir, case_id_str, 1))  # channel 1
-    
-    # 1-channel labels
-    label_img.to_filename('{}/{}.nii.gz'.format(training_label_dir, case_id_str))
+    patient_map_d = {'training':[], 'test':[]}
 
-    patient_map_d['training'].append([f,case_id_str])
+    print('processing training set')
+    for c,f in enumerate(training_file_l):
+        print(f)
+        # split the patient file contents into channels
+        patient_arr = np.load(f)
+        patient_arr = np.moveaxis(patient_arr, 1, -1)  # move the z-axis to be last
+        
+        exh_img = nib.Nifti1Image(patient_arr[0], np.eye(4))  # identity matrix for transform
+        inh_img = nib.Nifti1Image(patient_arr[1], np.eye(4))
+        label_img = nib.Nifti1Image(patient_arr[4], np.eye(4))
+        
+        case_id_str = '{}_{:04d}'.format(task_name, c)
+        
+        # 2-channel inputs
+        exh_img.to_filename('{}/{}_{:04d}.nii.gz'.format(training_image_dir, case_id_str, 0))  # channel 0
+        inh_img.to_filename('{}/{}_{:04d}.nii.gz'.format(training_image_dir, case_id_str, 1))  # channel 1
+        
+        # 1-channel labels
+        label_img.to_filename('{}/{}.nii.gz'.format(training_label_dir, case_id_str))
 
-print('processing test set')
-for c,f in enumerate(test_file_l):
-    print(f)
-    # split the patient file contents into channels
-    patient_arr = np.load(f)
-    patient_arr = np.moveaxis(patient_arr, 1, -1)  # move the z-axis to be last
-    
-    exh_img = nib.Nifti1Image(patient_arr[0], np.eye(4))  # identity matrix for transform
-    inh_img = nib.Nifti1Image(patient_arr[1], np.eye(4))
-    label_img = nib.Nifti1Image(patient_arr[4], np.eye(4))
-    
-    case_id_str = '{}_{:04d}'.format(task_name, c)
-    
-    # 2-channel inputs
-    exh_img.to_filename('{}/{}_{:04d}.nii.gz'.format(test_image_dir, case_id_str, 0))  # channel 0
-    inh_img.to_filename('{}/{}_{:04d}.nii.gz'.format(test_image_dir, case_id_str, 1))  # channel 1
-    
-    # 1-channel labels
-    label_img.to_filename('{}/{}.nii.gz'.format(test_label_dir, case_id_str))
+        patient_map_d['training'].append([f,case_id_str])
 
-    patient_map_d['test'].append([f,case_id_str])
+    print('processing test set')
+    for c,f in enumerate(test_file_l):
+        print(f)
+        # split the patient file contents into channels
+        patient_arr = np.load(f)
+        patient_arr = np.moveaxis(patient_arr, 1, -1)  # move the z-axis to be last
+        
+        exh_img = nib.Nifti1Image(patient_arr[0], np.eye(4))  # identity matrix for transform
+        inh_img = nib.Nifti1Image(patient_arr[1], np.eye(4))
+        label_img = nib.Nifti1Image(patient_arr[4], np.eye(4))
+        
+        case_id_str = '{}_{:04d}'.format(task_name, c)
+        
+        # 2-channel inputs
+        exh_img.to_filename('{}/{}_{:04d}.nii.gz'.format(test_image_dir, case_id_str, 0))  # channel 0
+        inh_img.to_filename('{}/{}_{:04d}.nii.gz'.format(test_image_dir, case_id_str, 1))  # channel 1
+        
+        # 1-channel labels
+        label_img.to_filename('{}/{}.nii.gz'.format(test_label_dir, case_id_str))
 
-# write the dataset file
-generate_dataset_json(out_base_dir,
-                      channel_names={0: 'exh_ct', 1: 'inh_ct'},
-                      labels={
-                          'background': 0,
-                          'high function': 1,
-                          'medium function': 2,
-                          'low function': 3
-                      },
-                      num_training_cases=len(training_file_l),
-                      file_ending='.nii.gz',
-                      regions_class_order=(1, 2, 3),
-                      license='',
-                      reference='',
-                      dataset_release='1.0')
+        patient_map_d['test'].append([f,case_id_str])
 
-# write the patient ID mapping
-with open('patient-mapping.json', 'w') as fp:
-    json.dump(patient_map_d, fp)
+    # write the dataset file
+    generate_dataset_json(out_base_dir,
+                        channel_names={0: 'exh_ct', 1: 'inh_ct'},
+                        labels={
+                            'background': 0,
+                            'high function': 1,
+                            'medium function': 2,
+                            'low function': 3
+                        },
+                        num_training_cases=len(training_file_l),
+                        file_ending='.nii.gz',
+                        regions_class_order=(1, 2, 3),
+                        license='',
+                        reference='',
+                        dataset_release='1.0')
+
+    # write the patient ID mapping
+    with open('patient-mapping-exp-{}.json'.format(test_set_idx+1), 'w') as fp:
+        json.dump(patient_map_d, fp)
