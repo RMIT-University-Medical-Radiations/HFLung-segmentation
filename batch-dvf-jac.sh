238d2f12c7db981fed843d19839efd84087518a3
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2024-02-01 09:53:59 +1100

    Update batch-dvf-jac.sh

--- batch-dvf-jac.sh
+++ batch-dvf-jac.sh
@@ -4,7 +4,10 @@ patient_id=1
 while [ $patient_id -le 20 ]
     do
         printf -v pstr "%02d" $patient_id
-        echo "/datasets/RNSH_HFlung/pre-processed-plastimatch/mha/Patient$pstr"
+        cd "/datasets/RNSH_HFlung/pre-processed-plastimatch/mha/Patient$pstr"
+        echo "Processing Patient$pstr"
+        plastimatch register /HFLung-segmentation/register-commands.txt
+        plastimatch jacobian --input dvf.mha --output-img vf_jac.mha
         ((patient_id++))
     done
 echo All done
