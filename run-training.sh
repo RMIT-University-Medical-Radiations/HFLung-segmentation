619bdee407234a436fd03c3119ac4eff7a363bef
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2024-02-01 17:35:46 +1100

    Create run-training.sh

---
+++ run-training.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for training_set in {150..154}
+do
+    for config in 2d 3d_fullres 3d_lowres 3d_cascade_fullres
+    do
+        for fold in {1..5}
+        do
+                echo training set: $training_set, config: $config, fold $fold
+                nnUNetv2_train -tr nnUNetTrainerDA5_60epochs $training_set $config $fold -device cuda --npz
+                echo "-----------------------"
+        done
+    done
+done
