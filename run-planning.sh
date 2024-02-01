7a663ec94839801022d4d97dc1631c77ba3cb1e3
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2024-02-02 08:43:29 +1100

    Update run-planning.sh

--- run-planning.sh
+++ run-planning.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+readonly GPU_MEMORY=48
+
 for training_set in {150..154}
 do
-    nnUNetv2_plan_and_preprocess -d $training_set --verify_dataset_integrity -gpu_memory_target 48
+    nnUNetv2_plan_and_preprocess -d $training_set --verify_dataset_integrity -gpu_memory_target $GPU_MEMORY
 done
