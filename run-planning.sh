022dc1623cfb21b7395ac39bc450517be6eecf78
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2024-02-02 08:38:46 +1100

    Update run-planning.sh

--- run-planning.sh
+++ run-planning.sh
@@ -2,13 +2,5 @@
 
 for training_set in {150..154}
 do
-    for config in 2d 3d_fullres 3d_lowres 3d_cascade_fullres
-    do
-        for fold in {0..4}
-        do
-                echo training set: $training_set, config: $config, fold $fold
-                nnUNetv2_train -tr nnUNetTrainerDA5_60epochs $training_set $config $fold -device cuda --npz
-                echo "-----------------------"
-        done
-    done
+    nnUNetv2_plan_and_preprocess -d $training_set --verify_dataset_integrity -gpu_memory_target 48
 done
