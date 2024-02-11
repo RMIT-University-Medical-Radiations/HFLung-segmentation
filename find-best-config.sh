97ed8b23adea4d17a51150caba268f80db98e516
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2024-02-11 16:23:59 +1100

    Update find-best-config.sh

--- find-best-config.sh
+++ find-best-config.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-readonly CONFIGS_TO_CONSIDER="2d 3d_fullres 3d_lowres 3d_cascade_fullres"
+readonly CONFIGS_TO_CONSIDER="2d 3d_fullres 3d_lowres"
 
-for training_set in {150..154}
+while getopts t: flag
 do
-    nnUNetv2_find_best_configuration $training_set -tr nnUNetTrainerDA5_60epochs -c $CONFIGS_TO_CONSIDER
+    case "${flag}" in
+        t) training_set=${OPTARG};;
+    esac
 done
+
+nnUNetv2_find_best_configuration $training_set -tr nnUNetTrainerDA5_60epochs -c $CONFIGS_TO_CONSIDER
