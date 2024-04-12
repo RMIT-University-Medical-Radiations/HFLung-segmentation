09824ec94aa94e4bff1444ff27a21de77b38335f
Daryl Wilding-McBride <daryl.wilding-mcbride@rmit.edu.au>
2024-04-12 15:10:47 +1000

    Update run-planning.sh

--- run-planning.sh
+++ run-planning.sh
@@ -2,7 +2,7 @@
 
 readonly GPU_MEMORY=48
 
-for training_set in {160..164}
+for training_set in {160..162}
 do
     echo training set: $training_set
     nnUNetv2_plan_and_preprocess -d $training_set --verify_dataset_integrity -gpu_memory_target $GPU_MEMORY
