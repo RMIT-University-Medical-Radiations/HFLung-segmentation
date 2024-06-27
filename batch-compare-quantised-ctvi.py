db2d860627712373524fe13d1b62012dc19b2d57
Daryl Wilding-McBride <daryl.wilding-mcbride@rmit.edu.au>
2024-06-28 09:25:45 +1000

    Update batch-compare-quantised-ctvi.py

--- batch-compare-quantised-ctvi.py
+++ batch-compare-quantised-ctvi.py
@@ -7,28 +7,26 @@ NB_INPUT_DIR = '{}/repos/HFLung-segmentation'.format(expanduser('~'))
 NB_OUTPUT_DIR = '{}/papermill-output/compare-quantised-ctvi'.format(expanduser('~'))
 NOTEBOOK_NAME = 'compare nnunet predictions with quantised PET and CTVI-Jac'
 
-# make sure the notebook output directory exists
-if os.path.exists(NB_OUTPUT_DIR):
-    shutil.rmtree(NB_OUTPUT_DIR)
-    print('deleted {}'.format(NB_OUTPUT_DIR))
-os.makedirs(NB_OUTPUT_DIR)
-
 DATASET_IDS = [160,161,162]
 TEST_IDS = [0,1]
-# CONFIG_SUFFIXES = ['','/2d']
-CONFIG_SUFFIXES = ['/2d']
+CONFIG_SUFFIXES = ['','/2d']
 
 for config in CONFIG_SUFFIXES:
+    output_nb_path = '{}{}'.format(NB_OUTPUT_DIR, config.replace('/','-'))
+    # make sure the notebook output directory exists
+    if os.path.exists(output_nb_path):
+        shutil.rmtree(output_nb_path)
+        print('deleted {}'.format(output_nb_path))
+    os.makedirs(output_nb_path)
     for dataset_id in DATASET_IDS:
         for test_id in TEST_IDS:
             input_nb = '{}/{}.ipynb'.format(NB_INPUT_DIR, NOTEBOOK_NAME)
-            output_nb_path = '{}{}'.format(NB_OUTPUT_DIR, config)
             output_nb = '{}/{} - dataset id {:02d} - test id {:02d}.ipynb'.format(output_nb_path, NOTEBOOK_NAME, dataset_id, test_id)
 
             if not os.path.exists(output_nb_path):
                 os.makedirs(output_nb_path)
 
-            print('dataset id {:02d}, test id {:02d}: \'{}\''.format(dataset_id, test_id, output_nb))
+            print('dataset id {:03d}, test id {:02d}: \'{}\''.format(dataset_id, test_id, output_nb))
             pm.execute_notebook(
                                 input_path=input_nb,
                                 output_path=output_nb,
