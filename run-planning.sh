00a37235ade570c1752c5713d38d008c1b4b8003
Daryl Wilding-McBride <daryl.wilding-mcbride@rmit.edu.au>
2024-09-10 16:45:08 +1000

    Update run-planning.sh

--- run-planning.sh
+++ run-planning.sh
@@ -5,6 +5,6 @@ readonly GPU_MEMORY=48
 for training_set in {160..162}
 do
     echo training set: $training_set
-    nnUNetv2_plan_and_preprocess -d $training_set --verify_dataset_integrity -gpu_memory_target $GPU_MEMORY
+    nnUNetv2_plan_and_preprocess -d $training_set --verify_dataset_integrity -pl nnUNetPlannerResEncXL -gpu_memory_target $GPU_MEMORY -overwrite_plans_name nnUNetResEncUNetPlans_$GPU_MEMORY\G
     echo "-----------------------"
 done
