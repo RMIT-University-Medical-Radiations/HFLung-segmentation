f963094e16c26581a57e378431a29758d94e8f7b
Daryl Wilding-McBride <daryl.wilding-mcbride@rmit.edu.au>
2024-09-11 10:52:54 +1000

    Update run-training.sh

--- run-training.sh
+++ run-training.sh
@@ -2,27 +2,14 @@
 
 set -e
 
-for training_set in 161
+for training_set in 160
 do
     for config in 3d_fullres
     do
         for fold in {1..4}
         do
                 echo training set: $training_set, config: $config, fold $fold
-                nnUNetv2_train -tr nnUNetTrainerDA5_60epochs $training_set $config $fold -device cuda --npz
-                echo "-----------------------"
-        done
-    done
-done
-
-for training_set in 162
-do
-    for config in 3d_fullres
-    do
-        for fold in {0..4}
-        do
-                echo training set: $training_set, config: $config, fold $fold
-                nnUNetv2_train -tr nnUNetTrainerDA5_60epochs $training_set $config $fold -device cuda --npz
+                nnUNetv2_train -tr nnUNetTrainerDA5_60epochs -device cuda --npz -p nnUNetResEncUNetPlans_48G $training_set $config $fold
                 echo "-----------------------"
         done
     done
