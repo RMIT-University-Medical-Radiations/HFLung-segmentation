7ad296e1a64c9ba27b91d9ded01575b19960411f
Daryl Wilding-McBride <daryl.wilding-mcbride@rmit.edu.au>
2024-09-16 08:01:43 +1000

    Update run-inference.sh

--- run-inference.sh
+++ run-inference.sh
@@ -7,7 +7,7 @@ do
     for config in 3d_fullres
     do
         echo training set: $training_set, config: $config, fold $fold
-        nnUNetv2_predict -d Dataset\$training_set\_RNSH_HFlung -i /datasets/RNSH_HFlung/nnU-Net-processing/nnUNet_raw/Dataset\$training_set\_RNSH_HFlung/imagesTs -o /datasets/RNSH_HFlung/nnU-Net-processing/nnUNet_predictions/Dataset\$training_set\_RNSH_HFlung/best -f  0 1 2 3 4 -tr nnUNetTrainerDA5_60epochs -c 3d_fullres -p nnUNetResEncUNetPlans_48G -chk checkpoint_best.pth -device cuda
+        nnUNetv2_predict -d Dataset$training_set\_RNSH_HFlung -i /datasets/RNSH_HFlung/nnU-Net-processing/nnUNet_raw/Dataset$training_set\_RNSH_HFlung/imagesTs -o /datasets/RNSH_HFlung/nnU-Net-processing/nnUNet_predictions/Dataset$training_set\_RNSH_HFlung/best -f  0 1 2 3 4 -tr nnUNetTrainerDA5_60epochs -c 3d_fullres -p nnUNetResEncUNetPlans_48G -chk checkpoint_best.pth -device cuda
         echo "-----------------------"
     done
 done
