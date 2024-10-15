c706d51b82338ad6a1925fe5ff7304f848b4b45f
Daryl Wilding-McBride <daryl.wilding-mcbride@rmit.edu.au>
2024-10-15 16:33:50 +1100

    Update batch-compare-unquantised-ctvi.py

--- batch-compare-unquantised-ctvi.py
+++ batch-compare-unquantised-ctvi.py
@@ -18,7 +18,7 @@ NUMBER_OF_PATIENTS = 20
 for patient_id in range(1,NUMBER_OF_PATIENTS+1):
 
     input_nb = '{}/{}.ipynb'.format(NB_INPUT_DIR, NOTEBOOK_NAME)
-    output_nb = '{}/{} - patient id {:02d}.ipynb'.format(NB_OUTPUT_DIR, NOTEBOOK_NAME, patient_id)
+    output_nb = '{}/{} - pid {:02d}.ipynb'.format(NB_OUTPUT_DIR, NOTEBOOK_NAME, patient_id)
 
     print('patient id {:02d}: \'{}\' to \'{}\''.format(patient_id, input_nb, output_nb))
     pm.execute_notebook(
